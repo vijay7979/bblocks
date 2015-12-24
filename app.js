8ee88b667c0c6636a68eb21a2281a7b2710d49ef
vijay7979 <vijay@saturnsystems.com.my>
2015-12-24 23:02:44 +0800

    add initial tests

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
+	//throw 'error'
 	response.send('Ok');
 });
-app.listen(3000, function() {
-	console.log('Listening on port 3000');
-})
+
+// app.listen(3000, function() {
+// 	console.log('Listening on port 3000');
+// });
+
+module.exports = app;
