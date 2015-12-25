73b2f3d5bb19e260eac4bf4cfe4575acadaa73d2
vijay7979 <vijay@saturnsystems.com.my>
2015-12-25 17:57:57 +0800

    Listing cities on index.html

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('./public'));
+
 app.get('/', function(request, response) {
 	//throw 'error'
 	response.send('Ok');
+});
+
+//first end-point; making a request to '/cities'
+app.get('/cities', function(request, response) {
+	var cities = ['Lotopia', 'Caspiana', 'Indigo'];
+	response.send(cities);
 });
 
 // app.listen(3000, function() {
--- test.js
+++ test.js
@@ -11,11 +11,41 @@ describe('Requests to the root path', function () {
 			done();
 		});
 	});
+
+	it('Returns a HTML format', function(done) {
+		request(app)
+			.get('/')
+			.expect('Content-Type', /html/, done);
+	});
+
+	it('Returns an index file with Cities', function(done) {
+		request(app)
+			.get('/')
+			.expect(/cities/i, done);
+	});
 });
 
+//for our first end-point; making a request to "cities",
+//and getting back an array of cities.
+describe('Listing cities on /cities',function() {
+	it('Returns 200 status code', function(done) {
+		request(app)
+			.get('/cities')
+			.expect(200, done);	//a short-cut instead of calling the 'end' function
+	});
 
+	it('Returns JSON format', function(done) {
+		request(app)
+			.get('/cities')
+			.expect('Content-Type', /json/, done);	//where /json/ is a regular expression
+	});
 
-
+	it('Returns initial cities', function(done) {
+		request(app)
+			.get('/cities')
+			.expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done);
+	});
+});
 
 
 
